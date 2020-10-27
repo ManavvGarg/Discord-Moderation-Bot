1195cac647e9f0bb2f4f2d4cdf389b3dee82b8fe
ManavvGarg <58691392+ManavvGarg@users.noreply.github.com>
2020-10-27 23:14:03 +0530

    Update command.js

--- handler/command.js
+++ handler/command.js
@@ -9,5 +9,5 @@ module.exports = (bot) => {
             if (pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
           };
         };
-        ["fun", "dev", "anime", "giveaway", "misc", "mod", "utility", "music", "info", "emotion", "image", "economy"].forEach(x => load(x));
+        ["mod", "utility"].forEach(x => load(x));
 };
